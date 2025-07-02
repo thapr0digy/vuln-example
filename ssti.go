4286fb8a9dadeacd8d898b80616dc3dd7c3c9ed4
thapr0digy <lindomusic@gmail.com>
2025-07-01 23:15:19 -0400

    Add new rce

--- ssti.go
+++ ssti.go
@@ -100,3 +100,16 @@ func test_rce(userStr string) {
 	// Print the captured output
 	fmt.Printf("Command output:\n%s\n", out.String())
 }
+
+func VULN_RCE(userStr string) {
+	cmd := exec.Command(userStr)
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	// Run the command
+	err := cmd.Run()
+	if err != nil {
+		log.Fatalf("Command failed with error: %v", err)
+	}
+	// Print the captured output
+	fmt.Printf("Command output:\n%s\n", out.String())
+}
