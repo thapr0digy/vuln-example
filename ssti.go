6417e43b1fcbb444aa0e2b653c7b706d70197aab
thapr0digy <lindomusic@gmail.com>
2025-07-08 16:19:50 -0400

    testing two rces

--- ssti.go
+++ ssti.go
@@ -15,19 +15,6 @@ type User struct {
 	Password string
 }
 
-func test_rce2(userStr string) {
-	cmd := exec.Command(userStr)
-	var out bytes.Buffer
-	cmd.Stdout = &out
-	// Run the command
-	err := cmd.Run()
-	if err != nil {
-		log.Fatalf("Command failed with error: %v", err)
-	}
-	// Print the captured output
-	fmt.Printf("Command output:\n%s\n", out.String())
-}
-
 func match1(w http.ResponseWriter, req *http.Request) {
 
 	var user1 = &User{1, "user@gmail.com", "Sup3rSecr3t123!"}
@@ -78,6 +65,19 @@ func match2(w http.ResponseWriter, req *http.Request) {
 	tmpl.Execute(w, user1)
 }
 
+func test_rce(userStr string) {
+	cmd := exec.Command(userStr)
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	// Run the command
+	err := cmd.Run()
+	if err != nil {
+		log.Fatalf("Command failed with error: %v", err)
+	}
+	// Print the captured output
+	fmt.Printf("Command output:\n%s\n", out.String())
+}
+
 func no_match(w http.ResponseWriter, req *http.Request) {
 
 	var user1 = &User{1, "user@gmail.com", "Sup3rSecr3t123!"}
@@ -99,4 +99,17 @@ func no_match(w http.ResponseWriter, req *http.Request) {
 		fmt.Println(err)
 	}
 	tmpl.Execute(w, user1)
+}
+
+func test_rce2(userStr string) {
+	cmd := exec.Command(userStr)
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	// Run the command
+	err := cmd.Run()
+	if err != nil {
+		log.Fatalf("Command failed with error: %v", err)
+	}
+	// Print the captured output
+	fmt.Printf("Command output:\n%s\n", out.String())
 }
